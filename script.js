b637df090cfe40f9efa0fb66f7591c2f2df7b456
Nach <nachogilgarzon@gmail.com>
2022-11-23 09:06:11 +0100

    js

---
+++ script.js
@@ -0,0 +1,21 @@
+function seleccionar(link){
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    opciones[5].className = "";
+    opciones[6].className = "";
+    opciones[7].className = "";
+    opciones[8].className = "";
+    opciones[9].className = "";
+    opciones[10].className = "";
+    opciones[11].className = "";
+    opciones[12].className = "";
+    opciones[13].className = "";
+    opciones[14].className = "";
+    opciones[15].className = "";
+    opciones[16].className = "";
+    link.className = "seleccionado";
+  }
