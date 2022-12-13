9c868580db706aa7a8ff6575d336993ed77aca51
Nach <nachogilgarzon@gmail.com>
2022-12-13 13:27:32 +0100

    horario

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
-// Seleccionador
-function seleccionar(link){
-    var opciones = document.querySelectorAll('#links  a');
-    opciones[0].className = "";
-    opciones[1].className = "";
-    opciones[2].className = "";
-    opciones[3].className = "";
-    opciones[4].className = "";
-    opciones[5].className = "";
-    opciones[6].className = "";
-    opciones[7].className = "";
-    opciones[8].className = "";
-    opciones[9].className = "";
-    opciones[10].className = "";
-    opciones[11].className = "";
-    opciones[12].className = "";
-    opciones[13].className = "";
-    opciones[14].className = "";
-    opciones[15].className = "";
-    opciones[16].className = "";
-    link.className = "seleccionado";
-}
-
-// Scroll inicio
+// Animación inicio
 gsap.registerPlugin(ScrollTrigger);
+
 gsap.utils.toArray('.section').forEach(section => {
   ScrollTrigger.create({
     trigger: section,
@@ -38,6 +16,16 @@ window.onclick = function(event){
     if (event.target == modal){
         modal.style.display = "none";
     }
+}
+
+// Menú responsive
+function responsiveMenu(){
+  var x = document.getElementById("nav");
+  if (x.className === ""){
+    x.className = "responsive";
+  } else {
+    x.className = "";
+  }
 }
 
 // Galería
