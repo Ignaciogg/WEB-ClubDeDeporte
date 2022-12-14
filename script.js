e6c5cb176b8b7398c37461df8ae57834fc58c638
Samuel Fernando Méndez Saldías <72949404+sfms97@users.noreply.github.com>
2022-12-14 09:04:08 +0100

    Registro y equipos

--- script.js
+++ script.js
@@ -12,9 +12,12 @@ gsap.utils.toArray('.section').forEach(section => {
 
 // Login
 var modal = document.getElementById("id01");
+var modal2 = document.getElementById("id02");
 window.onclick = function(event){
     if (event.target == modal){
         modal.style.display = "none";
+    } else if (event.target == modal2){
+        modal2.style.display = "none";
     }
 }
 
