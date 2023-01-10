84571b4068ba95317357e7ec3e865b3e429e4c3c
Nach <nachogilgarzon@gmail.com>
2023-01-10 09:35:38 +0100

    quitar gsap

--- main/index.php
+++ main/index.php
@@ -62,8 +62,6 @@
                 </div>
             </div>
         </section>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/gsap.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/ScrollTrigger.min.js"></script>
         <script src="script.js"></script>
     </body>
     <footer>
